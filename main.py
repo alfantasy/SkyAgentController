1a5477df5b824a6062dc8fc683777b1ca2065774
alfantasy <alfantasyfn@gmail.com>
2026-03-22 20:18:51 +0300

    Обновление #1.5 Введение файла updater.json и модифицирована работа main.py в зависимости от рабочей директории.

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from routers import auth, utils, files, python_executor
+import sys, os
+
+print("Reconfigurate imports and system path to Backend.")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+if script_dir not in sys.path:
+    sys.path.insert(0, script_dir)
+
+try:
+    from routers import auth, utils, files, python_executor
+    print("Paths to routers configured.")
+except ImportError as e:
+    print(f"Error importing routers: {e}")
 
 app = FastAPI(openapi_url=False, docs_url=False, redoc_url=False, swagger_ui_oauth2_redirect_url=False)
 
